151b6c2b89d8a9e8116f37be6d9906a7143a65a4
iunmzi <jiananwang1106@gmail.com>
2016-09-24 00:26:09 +0800

    implement ReceiverService

---
+++ src/main/java/com/jiananwang/iot/service/ReceiverService.java
@@ -0,0 +1,56 @@
+package com.jiananwang.iot.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * This service receives incoming bytes, puts into queue and dequeue at the proper time
+ *
+ */
+@Service
+public class ReceiverService {
+    Logger logger = LoggerFactory.getLogger(ReceiverService.class);
+
+    /**
+     * Thread safe blocking queue for read and write. Note it receives byte[] instead of byte.
+     */
+    private BlockingQueue<byte[]> queue = new LinkedBlockingDeque<>();
+
+    /**
+     * Keep tracking size of elements in queue
+     */
+    private long size = 0;
+
+
+
+
+
+
+    public void put(byte[] bytes) {
+        if (bytes == null)
+            return;
+
+        try {
+            this.queue.put(bytes);
+            this.size += bytes.length;
+        }catch(Exception e) {
+            logger.debug(e.getMessage());
+        }
+    }
+
+    public byte[] pull() {
+        byte[] retval = this.queue.poll();
+        if (retval != null)
+            this.size -= retval.length;
+
+        return retval;
+    }
+
+    public long size() {
+        return this.size;
+    }
+}
