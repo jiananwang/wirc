6540d7afd27b726ae563f87dabce1c69984771c1
Jianan Wang <jiananwang1106@gmail.com>
2016-09-24 01:17:52 +0800

    consume buffer

--- src/main/java/com/jiananwang/iot/Initializr.java
+++ src/main/java/com/jiananwang/iot/Initializr.java
@@ -1,7 +1,10 @@
 package com.jiananwang.iot;
 
+import com.jiananwang.iot.service.UploadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Component;
 
 import com.jiananwang.iot.serial.SerialPortRW;
@@ -9,13 +12,17 @@ import com.jiananwang.iot.serial.SerialPortRW;
 @Component
 @Scope("prototype")
 public class Initializr {
-	@Autowired
-	private SerialPortRW serialProtRW;
-	
+	@Autowired private SerialPortRW serialProtRW;
+
+	@Autowired private TaskExecutor taskExecutor;
+
+	@Autowired private ApplicationContext appContext;
 	
 	public void init() {
 		try {
 			serialProtRW.connect("/dev/ttyUSB0");
+			UploadService service = new UploadService(appContext);
+			taskExecutor.execute(service);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/main/java/com/jiananwang/iot/service/UploadService.java
@@ -0,0 +1,44 @@
+package com.jiananwang.iot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by root on 16-9-24.
+ */
+@Service
+@PropertySource("application.properties")
+public class UploadService implements Runnable {
+    private ApplicationContext appContext;
+
+    @Value("${wircUploadInterval}") private int uploadInterval;
+
+
+
+    public UploadService(ApplicationContext context) {
+        this.appContext = context;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            BytesQueueService bytesQueueService = (BytesQueueService)appContext.getBean("bytesQueueService");
+            if (bytesQueueService != null) {
+                if (bytesQueueService.size() > 10) {
+                    bytesQueueService.pull();
+                    System.out.println();
+                    System.out.println(" queue size: " + bytesQueueService.size());
+                }
+            }
+
+            try {
+                Thread.sleep(uploadInterval);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
