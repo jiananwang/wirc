8c5739cba008271f26d6d17c1b89ce451b35057b
Jianan Wang <jiananwang1106@gmail.com>
2016-10-07 01:25:46 +0800

    consolidate commands

--- src/main/java/com/jiananwang/iot/constant/ImpinjCommands.java
+++ src/main/java/com/jiananwang/iot/constant/ImpinjCommands.java
@@ -5,4 +5,24 @@ package com.jiananwang.iot.constant;
  */
 public class ImpinjCommands {
     public static final byte SET_ANTENNA = (byte) 0x74;
+
+    /**
+     * Set current working antenna
+     * @return
+     */
+    public static byte[] newSetAntenna() {
+        return new byte[]{(byte) 0xA0, 0x04, 0x01, (byte) 0x74, 0x03, 0x00};
+    }
+
+    /**
+     * Read fron device into buffer
+     * @return
+     */
+    public static byte[] newReadIntoBuffer() {
+        return new byte[] { (byte)0xA0, 0x04, 0x01, (byte)0x80, 0x0A, (byte)0xD1 };
+    }
+
+    public static byte[] newReadFromBuffer() {
+        return new byte[] { (byte)0xA0, 0x03, 0x01, (byte)0x90, (byte)0xCC};
+    }
 }
--- src/main/java/com/jiananwang/iot/serial/SerialWriter.java
+++ src/main/java/com/jiananwang/iot/serial/SerialWriter.java
@@ -3,11 +3,11 @@ package com.jiananwang.iot.serial;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import com.jiananwang.iot.constant.ImpinjCommands;
 import com.jiananwang.iot.registery.GlobalRegistry;
 import com.jiananwang.iot.util.CheckSum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.ContextLoader;
@@ -56,7 +56,7 @@ public class SerialWriter implements Runnable {
 				try {
 					// 1. set antenna
 					if (globalRegistry.getAntenna() < 0) {
-						byte[] b = new byte[] { (byte)0xA0, 0x04, 0x01, (byte)0x74, 0x03, 0x00 };
+						byte[] b = ImpinjCommands.newSetAntenna();
 //						byte[] b = new byte[] { (byte)0xA0, 0x04, 0x01, (byte)0x80, 0x0A, (byte)0xD1 };
 						byte check_sum = CheckSum.impinjCheckSum(b, 5);
 						b[5] = check_sum;
@@ -68,11 +68,13 @@ public class SerialWriter implements Runnable {
 						continue;
 					}
 
-					byte[] b = new byte[] { (byte)0xA0, 0x04, 0x01, (byte)0x80, 0x0A, (byte)0xD1 };
+					byte[] b = ImpinjCommands.newReadIntoBuffer();
+//					byte[] b = new byte[] { (byte)0xA0, 0x04, 0x01, (byte)0x80, 0x0A, (byte)0xD1 };
 					this.out.write(b); logger.debug("read into buffer");
 
 					Thread.sleep(5000);
-					b = new byte[] { (byte)0xA0, 0x03, 0x01, (byte)0x90, (byte)0xCC};
+					b = ImpinjCommands.newReadFromBuffer();
+//					b = new byte[] { (byte)0xA0, 0x03, 0x01, (byte)0x90, (byte)0xCC};
 					this.out.write(b); logger.debug("read from buffer");
 
 					Thread.sleep(1000);
